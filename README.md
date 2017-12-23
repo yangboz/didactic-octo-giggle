e1c87863bef27f2724de0fee14cd7fdfda450e13
yangboz <youngwelle@gmail.com>
2017-12-23 11:00:01 +0800

    ADDED:router handlers testing functions;

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # didactic-octo-giggle
 Golang with Kubernetes from Zero to One  
 ## Reference
-http://mp.weixin.qq.com/s/2LGPmyZToeNmZ8G9trOi3A
+https://blog.gopheracademy.com/advent-2017/kubernetes-ready-service/
--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
-import "fmt"
 import (
-	"net/http"
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rumyantseva/advent-2017/handlers"
+	"github.com/rumyantseva/advent-2017/version"
 )
 
+// How to try it: PORT=8000 go run main.go
 func main() {
-	log.Print("Starting the service...")
-	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w,"Hello!Your request was processed.")
-	},)
+	log.Printf(
+		"Starting the service...\ncommit: %s, build time: %s, release: %s",
+		version.Commit, version.BuildTime, version.Release,
+	)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
+	r := handlers.Router(version.BuildTime, version.Commit, version.Release)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000",nil))
+
+	killSignal := <-interrupt
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("Done")
 }
-
-
